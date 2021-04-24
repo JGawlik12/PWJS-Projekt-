335d619060c1023bece93b9034ac00c6f8f47a40
JGawlik12 <76133527+JGawlik12@users.noreply.github.com>
2021-04-24 14:02:41 +0200

    Update zadania.py

--- zadania.py
+++ zadania.py
@@ -2,13 +2,20 @@
 
 
 #zad1
+
 print("hello world!")
   
+  
+  
 #zad2
+
 personInfo = raw_input("date of birth: ")
 print(personInfo)
   
+  
+  
 #zad3
+
 code = input("code: ")
 code2 = input("code2: ")
 if(code2 == code):
@@ -16,7 +23,10 @@ if(code2 == code):
 else:
     print("codes error")
   
+  
+  
 #zad4
+
 import os
  
 dir = r"C:\Users\Jarek\Documents\Python"
@@ -24,12 +34,18 @@ lista = os.listdir(dir)
 number_files = len(lista)
 print number_files
   
+  
+  
 #zad5
+
 import os
 for root, dirs, files in os.walk(r'C:\Users\Jarek\Documents\Python'): 
     print root, dirs, files
   
+  
+  
 #zad6
+
 import os
 os.chdir(r'C:\Users\Jarek\Documents\Python')  
 old = ''
@@ -42,7 +58,10 @@ for file in os.listdir(r'C:\Users\Jarek\Documents\Python'):
         print(new[0]+".png")
         os.rename(old , new[0]+'.png')
   
+  
+  
 # #zad7
+
 import os
 os.chdir(r'C:\Users\Jarek\Documents\Python')
 infile = os.path.join(os.getcwd()) + '\\' + "nowomowa.txt"
@@ -57,7 +76,9 @@ line = line.replace(word, "")
 fout.write(line)
 
 
+
 #zad8
+
 import os
 os.chdir(r'C:\Users\Jarek\Documents\Python')
 infile = os.path.join(os.getcwd()) + '\\' + "nowomowa.txt"
@@ -72,7 +93,9 @@ line = line.replace(key, replaceDict[key])
 fout.write(line)
   
   
+  
 #zad9
+
 import math
 from math import sqrt
  
@@ -95,7 +118,9 @@ else:
     print("brak rozwiazan")
   
   
+  
 #zad10
+
 import random
 L = []
 for i in range(50):
@@ -113,7 +138,10 @@ P.sort()
 P.reverse()
 print(P)
   
+  
+  
 #zad11
+
 a = [1, 2, 12, 4] 
 b = [2, 4, 2, 8]
 c = []
@@ -121,7 +149,10 @@ for i in range(len(a)):
     c.append(a[i]*b[i])
 print(c)
   
+  
+  
 #zad12
+
 N = 128
 import random
 L1 = [[random.random() for i in range(N)] for j in range(N)]
@@ -133,7 +164,9 @@ for i in range(N):
         L3[i].append(L1[i][j] + L2[i][j])
   
   
+  
 #zad13
+
 N = 8
 import random
 L1 = [[random.random() for i in range(N)] for j in range(N)]
@@ -145,8 +178,34 @@ for i in range(len(L1)):
             L3[i][j] += L1[i][k] * L2[k][j]  
 for r in L3:  
     print(r)
-       
-#zad14
+      
+      
+
+#zad14   
+
+#WYZNACZNIK MACIERZY
+
+import random as rd
+import numpy as np
+
+L = []
+rd.seed()
+
+def getDet(L, m):
+    for i in range(0,7):
+    L.append([])
+    for j in range(0,7):
+        L[i].append(rd.random()*m)
+
+    return np.linalg.det(L)
+
+print(L)
+print(getDet(L, 999))
+    
+      
+         
+#zad15
+
 import math
 from math import sqrt
   
@@ -181,8 +240,10 @@ class Complex(object):
         new = (self.real**2 + (self.imag**2)*-1)
         return Complex(sqrt(new.real))
       
- 
-#zad 15
+      
+      
+#zad 16
+
 while True:
     
     choice = str(input("Enter choice(1/2/3/4): "))
@@ -213,7 +274,10 @@ while True:
     else:
         print("Invalid Input")
   
-#zad16
+  
+  
+#zad17
+
 import os
 os.chdir(r'C:\Users\Jarek\Documents\Python')  
 import xml.etree.ElementTree as xml
@@ -243,36 +307,11 @@ with open("myfile_modified.xml", "wb") as fh:
     tree.write(fh)
   
   
-#zad16
+  
+#zad18
+
 import os
-os.chdir(
-import os
-os.chdir(r'C:\Users\Jarek\Documents\Python')
-infile = os.path.join(os.getcwd()) + '\\' + "nowomowa.txt"
-outfile = os.path.join(os.getcwd()) + '\\' + "nowomowa_zmieniony.txt"
-
-delete = ["się", " i ", "nigdy", "dlaczego"]
-with open(infile, encoding="utf-8") as fin, open(outfile, "w+", encoding="utf-8") as fout:
-for line in fin:
-for word in delete:
-line = line.replace(word, "")
-fout.write(line)
-
-
-#zad8
-import os
-os.chdir(r'C:\Users\Jarek\Documents\Python')
-infile = os.path.join(os.getcwd()) + '\\' + "nowomowa.txt"
-outfile = os.path.join(os.getcwd()) + '\\' + "nowomowa_zmieniony.txt"
-
-replaceDict = {"się":"oraz", " i ":" i ", "nigdy":"prawie nigdy", "dlaczego":"czemu"}
-
-with open(infile, encoding="utf-8") as fin, open(outfile, "w+", encoding="utf-8") as fout:
-for line in fin:
-for key in replaceDict.keys():
-line = line.replace(key, replaceDict[key])
-fout.write(line)
-) 
+os.chdir(r'C:\Users\Jarek\Documents\Python') 
 import json
   
 movie_dict =  {"Title": "Star Wars",
@@ -294,7 +333,37 @@ with open(os.getcwd()+'\person.json', 'w') as f:
     json.dump(data, f)
 print(data)
   
-# zad17
+  
+  
+#zad19
+
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
+
+mu = 100 
+sigma = 15 
+x = mu + sigma * np.random.randn(10000)
+
+num_bins = 20
+
+n, bins, patches = plt.hist(x, num_bins, normed=1, facecolor='blue', alpha=0.5)
+
+
+y = mlab.normpdf(bins, mu, sigma)
+plt.plot(bins, y, 'r--')
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
+
+
+plt.subplots_adjust(left=0.15)
+plt.show()
+   
+  
+#zad20
+
 import threading
 import random
 import time
@@ -356,53 +425,8 @@ main()
 
 
 
-#WYZNACZNIK MACIERZY
-
-import random as rd
-import numpy as np
-
-L = []
-rd.seed()
-
-def getDet(L, m):
-    for i in range(0,7):
-    L.append([])
-    for j in range(0,7):
-        L[i].append(rd.random()*m)
-
-    return np.linalg.det(L)
-
-print(L)
-print(getDet(L, 999))
 
 
-
-
-#HISTOGRAM
-
-import numpy as np
-import matplotlib.mlab as mlab
-import matplotlib.pyplot as plt
-
-
-mu = 100 
-sigma = 15 
-x = mu + sigma * np.random.randn(10000)
-
-num_bins = 20
-
-n, bins, patches = plt.hist(x, num_bins, normed=1, facecolor='blue', alpha=0.5)
-
-
-y = mlab.normpdf(bins, mu, sigma)
-plt.plot(bins, y, 'r--')
-plt.xlabel('Smarts')
-plt.ylabel('Probability')
-plt.title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
-
-
-plt.subplots_adjust(left=0.15)
-plt.show()
 
 
 
